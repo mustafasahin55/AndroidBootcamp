3540d32f900c77e17b30df6ee0f3779364eeae93
Mustafa Şahin <mustafasahin8484@gmail.com>
2024-08-10 20:40:30 +0300

    Odev2 eklendi

--- app/src/main/java/com/example/myapplication/nesne_tabanli/Odev_2/Odev2.kt
+++ app/src/main/java/com/example/myapplication/nesne_tabanli/Odev_2/Odev2.kt
@@ -1,4 +1,59 @@
 package com.example.myapplication.nesne_tabanli.Odev_2
 
 class Odev2 {
+
+    fun icAciHesapla(kenarSayisi:Int):Int{
+        return (kenarSayisi-2)*180
+    }
+
+    fun maasHesapla(gun:Int): Double{
+        var toplamSaat = gun * 8
+
+        if (toplamSaat > 160){
+            var kalanSaat = toplamSaat - 160
+            var maas :Double = (160*10 + kalanSaat*20).toDouble()
+            return maas
+
+        }else{
+            var maas :Double = (toplamSaat * 10).toDouble()
+            return maas
+
+        }
+
+    }
+
+    fun kotaHesapla(kota:Double):Double{
+
+        if(kota>50){
+            var asim = kota - 50
+            return ((asim*4)+100).toDouble()
+        }else{
+            return 100.toDouble()
+        }
+    }
+
+    fun derece2Fahreneit(derece:Double):Double{
+
+        return (derece*1.8 + 32)
+
+    }
+
+    fun d4genCevre(kenar1:Double,kenar2:Double):Double{
+        return (kenar1+kenar2)*2
+    }
+
+    fun faktoriyel(sayi:Int):Long{
+        var sonuc:Long = 1
+
+        for (x in sayi downTo 1 step 1 ){
+            sonuc *= x
+
+        }
+        return sonuc
+    }
+
+    fun aHarfiSay(input: String): Int {
+        return input.count { it == 'a' }
+    }
+
 }
--- app/src/main/java/com/example/myapplication/nesne_tabanli/Odev_2/Odev2Main.kt
+++ app/src/main/java/com/example/myapplication/nesne_tabanli/Odev_2/Odev2Main.kt
@@ -1,2 +1,31 @@
 package com.example.myapplication.nesne_tabanli.Odev_2
 
+fun main() {
+    var test = Odev2()
+
+    //iç açı hesaplayıcı
+    print("Altıgenin İç Açıları: "); println(test.icAciHesapla(6))
+    println("-------------------------------------------------------------")
+    //Maaş Hesabı
+    print("12 Gün Çalışma Maaş: "); println(test.icAciHesapla(12))
+    print("28 Gün Çalışma Maaş: "); println(test.icAciHesapla(28))
+    println("-------------------------------------------------------------")
+    //Kota Hesaplayıcı
+    print("45GB Ücreti: "); println(test.kotaHesapla(45.3))
+    print("98GB Ücreti: "); println(test.kotaHesapla(98.2))
+    println("-------------------------------------------------------------")
+    //Derece Fahreneit çevirici
+    print("18 Derece Selsiyus Fahreneit Karşılığı: ");println(test.derece2Fahreneit(18.0))
+    println("-------------------------------------------------------------")
+    //Dikdörtgen Çevre
+    print("Kenarları 12 ve 17 birim olan dikdörtgenin çevresi:");println(test.d4genCevre(12.0,17.0))
+    println("-------------------------------------------------------------")
+    //Faktoriyel Hesaplama
+    print("4!: ");println(test.faktoriyel(4))
+    println("-------------------------------------------------------------")
+    //A harf saydırma
+    print("araba kelimesindeki a harfi sayısı: ");println(test.aHarfiSay("araba"))
+    println("-------------------------------------------------------------")
+
+
+}
